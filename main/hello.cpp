1dd9272630bf247ffee896deb8a26ad2d702f0ea
ShreevathsaGP <gpshreevathsa@gmail.com>
2025-03-13 16:42:12 +0530

    pes1ug22cs568 change :)

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "PES1UG22CS568: Hello, World\n";
   cout << "PES1UG22CS568: Hello, Jenkins\n";
   cout << "PES1UG22CS568: I have successfully built and run this  \n";
-  cout << "SOMETHING HAS CHANGED!\n";
+  cout << "SOMETHING HAS CHANGED 123!\n";
   return 0;
 }
