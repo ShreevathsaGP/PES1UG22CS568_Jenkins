3b37028ebaa26c18558b85e7b3b384d22771b01e
ShreevathsaGP <gpshreevathsa@gmail.com>
2025-03-13 15:39:39 +0530

    pes1ug22cs568 correct!

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "PES1UG22CS568: Hello, World\n";
+  cout << "PES1UG22CS568: Hello, Jenkins\n";
+  cout << "PES1UG22CS568: I have successfully built and run this  \n";
   return 0;
 }
